75be7baa11b273e69a9c614e3573bf756865da76
John F. Lake <jlake@Johns-MacBook-Pro.local>
2016-02-04 22:53:31 -0500

    Basic checking and word adding is correct.

--- fc.py
+++ fc.py
@@ -5,20 +5,22 @@
 
 
 
-def store_word(word):
-
-
-	file = open('words.txt','a+'); 
-	
+def store_vocab_word(word):
+	file = open('fcData/words.txt','a+'); 
 	file.write(word); 
 	file.write("\n"); 
-
 	file.close(); 
 
 
 def options(): 
-
 	print "Options go here."
+	print "Enter 1 to add a word.\n"
+	print "Enter 2 to study.\n"
+	print "Enter 3 to delete a word.\n"
+		
+	return int(raw_input(""))
+	
+
 
 
 
@@ -28,13 +30,43 @@ def main():
 	print "\n\n"
 	print "WELCOME to the Deutsch Flash Card Simulator!  Please Enter Your option for study today!"
 	print "\n\n"
-	options(); 
+	opt = options(); 
+
+
+	if opt is 1: 
+		print "Enter your vocabulary word for adding to the word list."
+		word = raw_input("Format: word,definition\n")
+		store_vocab_word(word); 
+	elif opt is 2: 
+		print "Study time!"
+		end = 0
+		words = {}
+
+		try: 
+			with open('fcData/words.txt','r') as file:
+				for line in file: 
+					wordDef = line.split(",")
+					word = wordDef[0]
+					d = wordDef[1]
+					words[word] = d
+			file.close()
+		except: 
+			print "There's a problem with your word list...shit."
+		for key in words: 
+			print words[key]
+			resp = raw_input("")
+			if resp.lower() in key.lower(): 
+				print "RIGHT!"
+			else: 
+				print "WRONG!"
+				
+
+				
 
 
 
-	word = raw_input("Enter a word for testing purposes.")
 
-	store_word(word); 
+
 
 
 
